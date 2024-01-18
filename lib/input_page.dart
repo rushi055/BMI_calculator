4c70a7dd54f03e951560f9899c09199e7891c136
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-01-19 02:52:06 +0530

    custom widget complete

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,31 +6,63 @@ class InputPage extends StatefulWidget {
   @override
   State<InputPage> createState() => _InputPageState();
 }
+
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Calculator'),
+        title: const Text('BMI Calculator'),
       ),
-      body: Column(
-        children:[
-          Expanded(child: Row(
-            children: [
-              Expanded(child: Container()),
-              Expanded(child: Container()),
-            ],
-          )),
-
-          Expanded(child: Container()),
-
-          Expanded(child: Row(
-            children: [
-              Expanded(child: Container()),
-              Expanded(child: Container()),
-            ],
-          ))
+      body: const Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(colour: Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+
+//Specialise Widget to reuse the set of code
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
